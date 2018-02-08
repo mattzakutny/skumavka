21e5330b3b93bfd999d2d7b72942039202761ccf
Matt Zakutny <matt.zakutny@gmail.com>
2018-02-08 14:35:08 -0800

    routes

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ const bodyParser = require('body-parser');
 app.listen(port, () => {
     console.log("App up and running on" + port);
 });
+
+// handle incoming requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// middleware to handle wrong routes 
+app.use( (req, res) => {
+    res.status(404).send({ url: req.originalUrl + 'not found' });
+});
+
+let routes = require("./api/routes/skumavkaRoutes");
+routes(app); // register our routes
+
+app.listen(port); 
+console.log('App running on ' + port);
