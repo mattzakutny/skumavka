8c9e80b5db4c5609612a415c0580365a869ecf98
Matt Zakutny <matt.zakutny@gmail.com>
2018-02-08 16:58:25 -0800

    update routes

--- api/routes/skumavkaRoutes.js
+++ api/routes/skumavkaRoutes.js
@@ -1,6 +1,6 @@
 // api/models/skumavkaRoutes.js
 module.exports = (app) => {
-    let skumavka = require('../controllers/skumavkaController');
+    let skumavka = require('../controllers/skumavkaController.js');
     // our Routes
     app.route('/items')
         .post(skumavka.createItem);
